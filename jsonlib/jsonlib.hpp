189622f9f853f7f4ed275383251f6edf0afa7339
uchenily <uchenily@qq.com>
2024-05-24 09:51:17 +0800

    Encapsulate type and data into `Value`

--- jsonlib/jsonlib.hpp
+++ jsonlib/jsonlib.hpp
@@ -29,50 +29,161 @@ class Json {
 
     using enum Type;
 
+    struct Value {
+        Type type_;
+        Data data_;
+
+        Value()
+            : type_{Null} {}
+
+        Value(bool value)
+            : type_{value ? True : False} {}
+
+        auto operator==(const Value &another) const noexcept -> bool {
+            if (this->type_ != another.type_) {
+                return false;
+            }
+
+            switch (this->type_) {
+            case Null:
+            case True:
+            case False:
+                return true;
+            case String:
+                return this->data_.string_ == another.data_.string_;
+            case Number:
+                return this->data_.number_ == another.data_.number_; // ?
+            case Array:
+                return *this->data_.array_ == *another.data_.array_;
+            case Object:
+                return *this->data_.object_ == *another.data_.object_;
+            }
+        }
+
+        template <Type T>
+        auto is() -> bool {
+            return type_ == T;
+        }
+
+        template <Type T>
+        auto as() {
+            if constexpr (T == Null) {
+                type_ = Null;
+                return nullptr;
+            } else if constexpr (T == False) {
+                type_ = False;
+                return false;
+            } else if constexpr (T == True) {
+                type_ = True;
+                return true;
+            } else if constexpr (T == Number) {
+                type_ = Number;
+                return data_.number_;
+            } else if constexpr (T == String) {
+                type_ = String;
+                return data_.string_;
+            } else if constexpr (T == Array) {
+                if (!data_.array_) {
+                    data_.array_ = std::make_shared<array_t>();
+                }
+                type_ = Array;
+                return data_.array_;
+            } else if constexpr (T == Object) {
+                if (!data_.object_) {
+                    data_.object_ = std::make_shared<object_t>();
+                }
+                type_ = Object;
+                return data_.object_;
+            }
+        }
+
+        auto serialize_to(std::ostringstream &out) const -> void {
+            switch (type_) {
+            case Object:
+                serialize_object(out);
+                break;
+            case Array:
+                serialize_array(out);
+                break;
+            case String:
+                out << '"';
+                out << data_.string_;
+                out << '"';
+                break;
+            case Number:
+            case True:
+                out << "true";
+                break;
+            case False:
+                out << "false";
+                break;
+            case Null:
+                out << "null";
+                break;
+            default:
+                out << "?";
+            }
+        }
+
+    private:
+        auto serialize_object(std::ostringstream &out) const noexcept -> void {
+            auto        n = data_.object_->size();
+            std::size_t i{0};
+
+            out << '{';
+            for (auto &it : *data_.object_) {
+                out << '"' << it.first << '"';
+                out << ": ";
+                it.second.value_->serialize_to(out);
+                if (++i < n) {
+                    out << ',';
+                }
+            }
+            out << '}';
+        }
+
+        auto serialize_array(std::ostringstream &out) const noexcept -> void {
+            auto        n = data_.array_->size();
+            std::size_t i{0};
+
+            for (auto &it : *data_.array_) {
+                out << '[';
+                it.value_->serialize_to(out);
+                if (++i < n) {
+                    out << ',';
+                }
+            }
+            out << ']';
+        }
+    };
+
 public:
     Json()
-        : type_{Null} {}
+        : value_{std::make_unique<Value>()} {}
 
     Json(bool value)
-        : type_{((value) ? True : False)} {}
+        : value_{std::make_unique<Value>(value)} {}
 
     auto operator[](const std::string &key) -> Json & {
-        if (!is_object()) {
-            create_object();
-        }
-        auto value = data_.object_->find(key);
-        if (value != data_.object_->end()) {
+        // if (!value_->is<Object>()) {
+        //     value_->as<Object>();
+        // }
+        auto value = value_->as<Object>()->find(key);
+        if (value != value_->as<Object>()->end()) {
             return value->second;
         }
 
-        return (*data_.object_)[key];
+        return (*value_->as<Object>())[key];
     }
 
     auto operator==(const Json &another) const noexcept -> bool {
-        if (this->type_ != another.type_) {
-            return false;
-        }
-
-        switch (this->type_) {
-        case Null:
-        case True:
-        case False:
-            return true;
-        case String:
-            return this->data_.string_ == another.data_.string_;
-        case Number:
-            return this->data_.number_ == another.data_.number_; // ?
-        case Array:
-            return *this->data_.array_ == *another.data_.array_;
-        case Object:
-            return *this->data_.object_ == *another.data_.object_;
-        }
+        return this->value_ == another.value_;
     }
 
 public:
     auto serialize() const -> std::string {
         std::ostringstream out;
-        serialize_to(out);
+        value_->serialize_to(out);
         return out.str();
     }
 
@@ -81,98 +192,6 @@ public:
     }
 
 private:
-    auto is_object() const noexcept -> bool {
-        return type_ == Object;
-    }
-
-    auto is_array() const noexcept -> bool {
-        return type_ == Array;
-    }
-
-    auto is_string() const noexcept -> bool {
-        return type_ == String;
-    }
-    auto is_boolean() const noexcept -> bool {
-        return type_ == False || type_ == True;
-    }
-
-    auto is_null() const noexcept -> bool {
-        return type_ == Null;
-    };
-
-    auto is_number() const noexcept -> bool {
-        return type_ == Number;
-    }
-
-    // auto to_number() const noexcept -> double {
-    //     return data_.number_;
-    // }
-
-    auto create_object() noexcept -> void {
-        type_ = Object;
-        data_.object_ = std::make_shared<std::map<std::string, Json>>();
-    }
-
-    auto serialize_to(std::ostringstream &out) const -> void {
-        switch (type_) {
-        case Object:
-            serialize_object(out);
-            break;
-        case Array:
-            serialize_array(out);
-            break;
-        case String:
-            out << '"';
-            out << data_.string_;
-            out << '"';
-            break;
-        case Number:
-        case True:
-            out << "true";
-            break;
-        case False:
-            out << "false";
-            break;
-        case Null:
-            out << "null";
-            break;
-        default:
-            out << "?";
-        }
-    }
-
-    auto serialize_object(std::ostringstream &out) const noexcept -> void {
-        auto        n = data_.object_->size();
-        std::size_t i{0};
-
-        out << '{';
-        for (auto &it : *data_.object_) {
-            out << '"' << it.first << '"';
-            out << ": ";
-            it.second.serialize_to(out);
-            if (++i < n) {
-                out << ',';
-            }
-        }
-        out << '}';
-    }
-
-    auto serialize_array(std::ostringstream &out) const noexcept -> void {
-        auto        n = data_.array_->size();
-        std::size_t i{0};
-
-        for (auto &it : *data_.array_) {
-            out << '[';
-            it.serialize_to(out);
-            if (++i < n) {
-                out << ',';
-            }
-        }
-        out << ']';
-    }
-
-private:
-    Data data_;
-    Type type_;
+    std::unique_ptr<Value> value_;
 };
 } // namespace jsonlib

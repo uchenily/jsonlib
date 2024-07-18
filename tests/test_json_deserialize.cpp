6b6e1f1e6350af592cef3caa15cb7798acb7b914
uchenily <uchenily@qq.com>
2024-07-18 15:10:12 +0800

    Add complex test

--- tests/test_json_deserialize.cpp
+++ tests/test_json_deserialize.cpp
@@ -72,6 +72,62 @@ auto test_object() {
     ASSERT(json_string == obj.serialize());
 }
 
+auto test_complex() {
+    std::string json_string = R"({
+      "Table": "Farm",
+      "Columns": {
+        "farm_name": {
+          "type": "str",
+          "block_stats": {
+            "0": {
+              "num": 6,
+              "min": "Breakfast Lunch Dinner",
+              "max": "Incubator",
+              "min_len": 9,
+              "max_len": 22
+            }
+          },
+          "num_blocks": 1,
+          "start_offset": 0
+        },
+        "min_age_weeks": {
+          "type": "float",
+          "block_stats": {
+            "0": {
+              "num": 6,
+              "min": 0,
+              "max": 52
+            }
+          },
+          "num_blocks": 1,
+          "start_offset": 192
+        },
+        "max_age_weeks": {
+          "type": "float",
+          "block_stats": {
+            "0": {
+              "num": 6,
+              "min": 2,
+              "max": 156
+            }
+          },
+          "num_blocks": 1,
+          "start_offset": 216
+        }
+      },
+      "Max Values Per Block": 50000
+    })";
+
+    auto obj = Json::deserialize(json_string);
+    LOG_INFO("obj.serialize(): {}", obj.serialize());
+    // ASSERT(json_string == obj.serialize());
+    ASSERT(obj["Table"].string() == "Farm");
+    ASSERT(obj["Columns"]["farm_name"]["type"].string() == "str");
+    ASSERT((int) obj["Columns"]["farm_name"]["block_stats"]["0"]["num"].number()
+           == 6);
+    ASSERT((int) obj["Max Values Per Block"].number() == 50000);
+}
+
 auto main() -> int {
     test_null();
     test_boolean();
@@ -80,4 +136,5 @@ auto main() -> int {
     test_string_with_escaped_char();
     test_array();
     test_object();
+    test_complex();
 }
